c8d748d290bba3bb084da90f14668095a10c8caf
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-09-13 20:46:04 -0500

    preparando pantallas para incluir componentes

---
+++ src/Contenido/Presentación/fotoDescripcion.jsx
@@ -0,0 +1,21 @@
+
+import { makeStyles } from '@material-ui/styles'
+
+const estilos = makeStyles(theme =>({
+
+
+
+}))
+
+
+const fotoDescripcion = () => {
+    const classes = estilos();
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default fotoDescripcion
---
+++ src/Contenido/Presentación/navbar.jsx
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/styles'
+
+const estilos = makeStyles(theme =>({
+
+
+
+}))
+
+
+const navbar = () => {
+    const classes = estilos();
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default navbar
