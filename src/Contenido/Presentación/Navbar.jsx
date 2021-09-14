d726535e6c8a20d4f77f2805adb258d56043bdc9
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-09-13 21:01:35 -0500

    navbar

--- src/Contenido/Presentación/Navbar.jsx
+++ src/Contenido/Presentación/Navbar.jsx
@@ -1,4 +1,11 @@
 import { makeStyles } from '@material-ui/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import Button from '@material-ui/core/Button'
+import FaceRoundedIcon from '@material-ui/icons/FaceRounded';
+
 
 const estilos = makeStyles(theme =>({
 
@@ -12,7 +19,27 @@ const Navbar = () => {
 
     return (
         <div>
-            <h1>holaxd</h1>
+            <AppBar position="fixed">
+
+                <Toolbar>                  
+                
+                    <Typography variant="h6">
+                        Portfolio Gustavo Rodriguez Calzada
+                    </Typography>
+
+                    <IconButton 
+                        aria-label="menu" 
+                        color="inherit"                         
+                    >
+                        <FaceRoundedIcon/>
+                    </IconButton>
+
+                    <Button color="inherit">Login</Button>                    
+                      
+
+                </Toolbar>
+
+            </AppBar>
         </div>
     )
 }
